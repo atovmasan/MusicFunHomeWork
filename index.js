7e5c4ad09a2791a73f44a4e434f292075b859ecd
atovmasan <yt988998@gmail.com>
2025-09-24 18:51:45 +0300

    1st lessonn

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const tasks = [
-  { title: "Купить продукты на неделю", isDone: false },
-  { title: "Полить цветы", isDone: true },
-  { title: "Сходить на тренировку", isDone: false },
-] s
+  { title: "Buy food", isDone: false },
+  { title: "Make a flower", isDone: true },
+  { title: "Go to gym", isDone: false },
+]
+
+
+const rootEl = document.getElementById("root")
+const headerEl = document.createElement("h1")
+headerEl.append("Tasks List")
+const ulEl = document.createElement("ul")
+
+
+ rootEl.append(headerEl)
+
+
+ tasks.forEach((task) => {
+    const divEl = document.createElement("div")
+    const liEl = document.createElement("li")
+    liEl.append(task.title)
+    const checbox = document.createElement("input")
+    checbox.type = 'checkbox'
+    checbox.checked = task.isDone
+
+    divEl.append(liEl)
+    divEl.append(checbox)
+
+    ulEl.append(divEl)
+ })
+
+rootEl.append(ulEl)
